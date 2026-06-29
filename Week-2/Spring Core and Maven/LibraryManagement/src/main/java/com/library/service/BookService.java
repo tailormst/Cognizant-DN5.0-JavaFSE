0b59a24ce435d4d28d7f278bc84cc0227857cc32
Mohammed Saifuddin Tailor <mohammedtailor5253@gmail.com>
2026-06-30 00:19:49 +0530

    Complete Week 2 Spring Core and Maven exercises 1, 2 and 4

---
+++ Week-2/Spring Core and Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    // Setter Injection
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void displayService() {
+        System.out.println("Book Service is working.");
+        repository.displayRepository();
+    }
+}
